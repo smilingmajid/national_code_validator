77c820a510a5380fbc33272b924b67b3be71525f
smilingmajid <majid.gha3mi.p@gmail.com>
2025-04-03 11:14:57 -0800

    test craeted

--- test/national_code_validator_test.dart
+++ test/national_code_validator_test.dart
@@ -2,15 +2,15 @@ import 'package:national_code_validator/national_code_validator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  test('Valid national codes', () {
+    expect(NationalCodeValidator.isValid("0079059747"), true);
+    expect(NationalCodeValidator.isValid("1234567893"), false);
+    expect(NationalCodeValidator.isValid("1111111111"), false);
+  });
 
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+  test('Invalid national codes', () {
+    expect(NationalCodeValidator.isValid("123"), false); // Too short
+    expect(NationalCodeValidator.isValid("abcdefghij"), false); // Contains letters
+    expect(NationalCodeValidator.isValid("0000000000"), false); // All digits are identical
   });
 }
